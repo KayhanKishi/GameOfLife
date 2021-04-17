022666a85176a45c62b9040b3ed90a1aa246249f
Abu Nadim <nadimkabir2@gmail.com>
2021-04-17 14:33:42 -0400

    changed the id for each cell in the table to be equal to the count variable

--- index.js
+++ index.js
@@ -1,29 +1,27 @@
-
 var rowNum = 30;
 var colNum = 50;
 
-let size = document.getElementById('gridSize');
+let size = document.getElementById("gridSize");
 
 gridLayout();
 
-
 function gridLayout() {
-    let gridLayout = document.getElementById('gridLayout');
-    let table = document.createElement("table");
-    
-    for (let i = 0; i < rowNum; i++) {
-        let tr = document.createElement("tr");
-        for (var j = 0; j < colNum; j++) {
-            let cell = document.createElement("td");
-            cell.setAttribute("id", i + "-" + j);
-            tr.appendChild(cell);
-        }
-        table.appendChild(tr);
+  let gridLayout = document.getElementById("gridLayout");
+  let table = document.createElement("table");
+  count = 0;
+  for (let i = 0; i < rowNum; i++) {
+    let tr = document.createElement("tr");
+    for (var j = 0; j < colNum; j++) {
+      let cell = document.createElement("td");
+      cell.setAttribute("id", count);
+      count++;
+      console.log(cell);
+      tr.appendChild(cell);
     }
-    gridLayout.appendChild(table);
+    table.appendChild(tr);
+  }
+  gridLayout.appendChild(table);
 }
-
-
 
 // Start everything
 window.onload = initialize;

ba2843d803c0d6d53d8e3c31bce81cf71381134c
Abu Nadim <nadimkabir2@gmail.com>
2021-04-17 16:28:08 -0400

    updates

--- index.js
+++ index.js
@@ -1,29 +1,34 @@
-
 var rowNum = 30;
 var colNum = 50;
-
-let size = document.getElementById('gridSize');
+var currentGen = [];
+console.log(document.cookie);
+let size = document.getElementById("gridSize");
 
 gridLayout();
 
-
+function main() {}
 function gridLayout() {
-    let gridLayout = document.getElementById('gridLayout');
-    let table = document.createElement("table");
-    
-    for (let i = 0; i < rowNum; i++) {
-        let tr = document.createElement("tr");
-        for (var j = 0; j < colNum; j++) {
-            let cell = document.createElement("td");
-            cell.setAttribute("id", i + "-" + j);
-            tr.appendChild(cell);
-        }
-        table.appendChild(tr);
+  let gridLayout = document.getElementById("gridLayout");
+  let table = document.createElement("table");
+  let count = 0;
+  for (let i = 0; i < rowNum; i++) {
+    let tr = document.createElement("tr");
+    for (var j = 0; j < colNum; j++) {
+      let cell = document.createElement("td");
+      let cellButton = document.createElement("button");
+      cellButton.setAttribute("class", "cellButton");
+      cellButton.setAttribute("id", count);
+      cellButton.setAttribute("onclick", "console.log(this)");
+      currentGen.push(cellButton);
+      count++;
+      cell.appendChild(cellButton);
+      tr.appendChild(cell);
     }
-    gridLayout.appendChild(table);
+    table.appendChild(tr);
+  }
+  gridLayout.appendChild(table);
+  console.log(currentGen);
 }
 
-
-
 // Start everything
-window.onload = initialize;
+// window.onload = initialize;

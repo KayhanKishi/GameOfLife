7ca2d01bcd5338f7d04067f5b1183d0b6e10b592
Furqaan Hyath <hyathfurqaan@gmail.com>
2021-04-17 18:45:56 -0400

    fixed index.js

--- index.js
+++ index.js
@@ -8,6 +8,12 @@ let nextGen = [rowNum*rowNum];
 
 gridLayout();
 createGenArrays();
+currentGen[0]=1;     //used for testing purposes to see if color function is working / 1 means cell is alive, 0 is dead
+
+
+// loop to continue running simulation
+updateGrid();
+color();
 
 function createGenArrays() {
     for (let i = 0; i < rowNum*rowNum; i++) {
@@ -15,17 +21,6 @@ function createGenArrays() {
         nextGen[i] = 0;
     }
 }
-
-
-// function initGenArrays() {
-//     for (let i = 0; i < rowNum; i++) {
-//         for (let j = 0; j < colNum; j++) {
-//             currentGen[i][j] = 0;
-//             nextGen[i][j] = 0;
-//         }
-//     }
-// }
-
 
 function gridLayout() {
     let gridLayout = document.getElementById('gridLayout');
@@ -38,7 +33,7 @@ function gridLayout() {
             let cell = document.createElement("td");
             cell.setAttribute("id", count);
             count++;
-            cell.setAttribute("class", "deadcell");
+   //         cell.setAttribute("class", "deadcell");
             cell.addEventListener("click", clicking);
             tr.appendChild(cell);
         }
@@ -48,33 +43,43 @@ function gridLayout() {
    
 }
 
-
+function color(){
+    for(let i=0; i<rowNum*rowNum; i++){
+        if(currentGen[i]==1){
+            document.getElementById(`${i}`).style.backgroundColor="red";
+        }
+        if(currentGen[i]==0){
+            document.getElementById(`${i}`).style.backgroundColor="gray";
+        }
+    }
+}
 
 function clicking() {
     let pos = this.id;
-    if (this.getAttribute("class") == "deadcell") {
-        this.setAttribute("class", "alivecell");
+    if (currentGen[pos] == 0) {
+        // this.setAttribute("class", "alivecell");
         currentGen[pos] = 1;
     }
     else {
-        this.setAttribute("class", "deadcell");
+        // this.setAttribute("class", "deadcell");
         currentGen[pos] = 0;
     }
     console.log(currentGen)
+    color();
 }
 
 function updateGrid(){
-    for (let i = 0; i < rowNum; i++) {
-        for (let j = 0; j < colNum; j++) {
-            let neighbors = applyRule(row, col);
-        }
-    }
+    for (let i = 0; i < rowNum*rowNum; i++) {
+        let newCell = applyRule(i);
 
-    
+        // newGen[i] = newCell
+    }
+    // set currentGen to newGen
+
 }
 
 function applyRule(){
-    
+    // game logic
 }
 
 
